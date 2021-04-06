d861707945144c6aa1a32cba219bec735e447db9
Jin-hw <70885954+Jin-hw@users.noreply.github.com>
2021-04-06 23:56:33 +0900

    add Cooment Schema

--- models/Image.js
+++ models/Image.js
@@ -17,7 +17,11 @@ const ImageSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
-    }
+    },
+    comments:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    }]
 })
 
 const model = mongoose.model("Image", ImageSchema);
